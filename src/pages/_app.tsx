1f3b5fc15f2c6d11a76e1a29424c236416bb962f
Reeves <reeves.abbas@gmail.com>
2022-05-04 00:55:15 -0400

    Colors, removed unnecessary div, and added text shadow support as well as fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
-      <div className='bg-lightestPurple dark:bg-darkestPurple min-h-[2000px]'>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </ThemeProvider>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,17 @@ module.exports = {
   darkMode: 'class',
   theme: {
     colors: {
-      'lightestPurple': '#DBD3EA',
-      'lightPurple': '#CCC0E7',
-      'darkerPurple': '#0F0026',
+      'lightestPurple': '#ECE7F8',
+      'lightPurple': '#DBD0F2',
+      'purple': '#733FE3',
+      'darkPurple': '#1A0144',
       'darkestPurple': '#080014',
-      'orange': '#EC4800',
-      'darkOrange': '#BD5635',
+      'lightOrange': '#FFD3C5',
+      'orange': '#FE5E19',
+      'darkOrange': '#9C2500',
       'darkerOrange': '#4D1700',
+      'lightestBlue': '#B9D1FF',
+      'lightBlue': '#7098FE',
       'blue': '#006FDC',
       'darkBlue': '#003F7D',
       'darkerBlue': '#002347',
@@ -22,6 +26,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
+        yeseva: 'Yeseva One',
         amiri: 'Amiri-Regular',
         fjalla: 'Fjalla-One'
       }
