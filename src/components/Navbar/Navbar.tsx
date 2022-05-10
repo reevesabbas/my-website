fb70ca02f127088fbb2c24409707282c58d8884d
Reeves <reeves.abbas@gmail.com>
2022-05-10 10:12:29 -0400

    edited navbar, added Skills and Contact containers

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,9 +5,11 @@ import { BsFillSunFill } from 'react-icons/bs'
 import { CgClose } from 'react-icons/cg'
 
 import TextButton from '../Buttons/TextButton'
+import Resume from '../Buttons/Resume';
+import Link from 'next/link';
 
 interface Props {
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 
 const Navbar: React.FC<Props> = () => {
@@ -17,7 +19,7 @@ const Navbar: React.FC<Props> = () => {
   const handleClick = () => setMenu(!menu);
   
   return (
-    <div className='fixed w-full h-[70px] md:h-[90px] flex justify-center items-center bg-lightestPurple dark:bg-darkestPurple z-10 md:drop-shadow-md dark:drop-shadow-none'>
+    <div className='fixed left-0 right-0 top-0 w-full h-[90px] flex justify-center items-center bg-lightestPurple dark:bg-darkestPurple z-10 drop-shadow-md dark:drop-shadow-none'>
 
       {/** Nav */}
 
@@ -25,15 +27,21 @@ const Navbar: React.FC<Props> = () => {
         <div className='pr-3 pl-5 pt-1 drop-shadow-lg'>
           {theme === 'dark' ? <BsFillSunFill size={30}> </BsFillSunFill> : <FaCloudMoon size={30} > </FaCloudMoon>}
         </div>
-        <div className='flex space-x-[200px] lg:space-x-[450px]'>
+        <div className='flex space-x-[450px]'>
             <TextButton onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
               {theme === 'dark' ? 'LIGHT' : 'DARK'}
             </TextButton>
           <div>
             <ul className='flex space-x-8'>
-              <li> <TextButton> CONTACT </TextButton> </li>
-              <li><TextButton> RESUME </TextButton> </li>
-              <li><TextButton> INTERESTS </TextButton> </li>
+              <a><TextButton> CONTACT </TextButton> </a>
+              <Link href='/Abbas_Reeves.pdf'> 
+                <a>   
+                  <TextButton>
+                    RESUME
+                  </TextButton>
+                </a>
+              </Link> 
+              <a><TextButton> INTERESTS </TextButton> </a>
             </ul>
           </div>
         </div>
@@ -44,14 +52,14 @@ const Navbar: React.FC<Props> = () => {
 
       <div className='md:hidden flex w-full justify-end pr-5 pt-2 z-10'>
         <button onClick={handleClick} className='hover:scale-110 hover:text-blue dark:hover:text-orange active:translate-y-0.5 active:text-darkBlue dark:active:text-darkerOrange ease-in-out duration-150 drop-shadow-lg'>
-          {!menu ? <FaBars size={30} /> : <CgClose size={35} /> }
+          {!menu ? <FaBars size={40} /> : <CgClose size={45} /> }
         </button>
       </div>
 
 
       {/**Mobile Nav */}
       
-      <div className={!menu ? 'hidden' : 'md:hidden absolute top-0 left-0 w-full h-screen bg-lightPurple dark:bg-darkerPurple flex flex-col justify-center items-center'}>
+      <div className={!menu ? 'hidden' : 'md:hidden absolute top-0 left-0 w-full h-screen bg-lightPurple dark:bg-darkPurple flex flex-col justify-center items-center'}>
         <ul className='space-y-6 text-center'>
           <li> <TextButton> HOME </TextButton></li>
           <li className='pb-3'> <TextButton onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,93 @@
 import React from 'react';
 import type { NextPage } from 'next'
-import Image from 'next/image'
+import { FaCopy } from 'react-icons/fa'
+import { FcSupport, FcBriefcase, FcFeedback } from 'react-icons/fc'
+import { SiTypescript, SiReact, SiHtml5, SiTailwindcss, SiJest, SiExpress } from 'react-icons/si'
 
-import { GrInstagram, GrTwitter, GrGithub } from 'react-icons/gr'
-import { FcBriefcase } from 'react-icons/fc'
-import { FaTools } from 'react-icons/fa'
 import Navbar from '../components/Navbar/Navbar';
-import IconButton from '../components/Buttons/IconButton';
 import GenContainer from '../components/Container/Gen';
+import SocialButtons from '../components/Buttons/SocialButtons';
 
 const Home: NextPage = () => {
   return (
-    <div className='w-full h-screen bg-lightestPurple dark:bg-darkestPurple'>
-      <Navbar> </Navbar>
+    <div className='content-container'>
 
-      {/** Container */}
-      <div className='md:max-w-[975px] h-[500px] flex mx-auto p-10 md:mb-10'>
-        <div className='flex flex-col justify-end space-y-3 text-center md:text-left'>
-          <h1 className='text-3xl md:text-5xl font-yeseva dark:text-white'> Reeves Abbas</h1>
+      <Navbar />
+
+      {/* Intro Container */}
+
+      <section className='flex px-10 mt-[200px]'>
+        <div className='flex flex-col justify-end space-y-5 text-center md:text-left'>
+          <h1 className='text-4xl md:text-5xl font-yeseva dark:text-white'> Reeves Abbas</h1>
           <h2 className='font-yeseva text-blue dark:text-orange text-2xl'> A Full Stack Developer </h2>
-          <p className='font-amiri text-center md:text-left text-lg font-semibold dark:font-medium'> Hey, welcome to my personal website; developed with 
-          NextJS, Typescript and Tailwind CSS. I'm a 20 year old student majoring in Software Engineering at the University of Michigan in the US. However,
-          I'm always seeking for more opportunities to nurture my talents and use them for creating software I am most
-          proud of. Thank you for visiting, I hope you like what you see! :-)
-
+          <p className='font-amiri text-center md:text-left text-xl font-semibold dark:font-medium leading-9'> Hey, welcome to my personal website; developed with 
+          NextJS, Typescript and Tailwind CSS. I'm a 20 year old student majoring in Software Engineering at the University of Michigan in the US.
+          I'm always seeking more opportunities to nurture my talents and use them for creating software I am
+          proud of. Thank you for visiting, I hope you like what you see!
           </p>
-          <div className='flex flex-row md:justify-start justify-center space-x-6'>
-            <a
-            href='https://twitter.com/reevfs'
-            target='_blank'
-            rel='noopener noreferrer'>
-              <IconButton icon={GrTwitter} >
-                <GrTwitter size={38}/> 
-              </IconButton>
-            </a>
-            <a
-            href='https://github.com/reevesabbas'
-            target='_blank'>
-              <IconButton icon={GrGithub}>
-                <GrGithub size={38} />
-              </IconButton>
-            </a>
-            <a
-            href='https://www.instagram.com/reeves.19/'
-            target='_blank'>
-              <IconButton icon={GrInstagram}>
-                <GrInstagram size={38} />
-              </IconButton>
-            </a>
-          </div>
+          <SocialButtons />
         </div>
-      </div>
+      </section>
       
 
-      {/* first container */}
+      {/* Skills Container */}
 
-      <div className='lg:max-w-[975px] flex mx-auto lg:p-0 p-10'>
-        <div className='w-[975px] h-fit flex mx-auto dark:bg-lightestBlue bg-darkPurple dark:bg-opacity-5 bg-opacity-10 rounded-lg p-10'>
-          <div className='w-full flex flex-col justify-center space-y-6 md:text-left text-center'>
-            <header className='flex flex-row justify-center md:justify-start'>
-              <h1 className='text-3xl md:text-4xl font-yeseva dark:text-white'> Skills </h1>
-              <FaTools size={35} className='w-fit h-fit pl-4 fill-orange' />
-            </header>
-            <p className='font-amiri text-lg font-semibold dark:font-medium'> Hi, words typing because I want to see how this looks for now. I will fill this in 
-            with words later. I just want to see how this looks. Okay so I'm just going to put more words here 
-            until I am satisfied. Done. Hi, words typing because I want to see how this looks for now. I will fill this in 
-            with words later. I just want to see how this looks. Okay so I'm just going to put more words here 
-            until I am satisfied. Done. </p>
-          </div>
+      <GenContainer title={'Skills'} icon={<FcSupport size={40}/>}>
+        These are just some of the dev tools I feel most confident with to use in my projects.
+        <div className='flex md:flex-row flex-col items-center text-center md:space-x-[85px] pt-8'>
+          <ul className='flex flex-row md:space-x-[85px] sm:space-x-[150px] space-x-[60px]'>
+            <li>
+              <SiTypescript size={80} fill='#007acc' />
+              <p className='pt-4'> Typescript </p>
+            </li>
+            <li>
+              <SiHtml5 size={80} fill='#e34c26' />
+              <p className='pt-4'> HTML </p>
+            </li>
+            <li>
+              <SiReact size={80} fill='#61dbfb' />
+              <p className='pt-4'> React </p>
+            </li>
+          </ul>
+          <ul className='flex flex-row md:space-x-[85px] sm:space-x-[150px] space-x-[60px] md:pt-0 pt-8'>
+            <li>
+              <SiJest size={80} fill='#a0444c'/>
+              <p className='pt-4'> Jest </p>
+            </li>
+            <li>
+              <SiTailwindcss size={80} fill='#34b4ec' />
+              <p className='pt-4'> Tailwind </p>
+            </li>
+            <li>
+              <SiExpress size={80} />
+              <p className='pt-4'> Express </p>
+            </li>
+          </ul>
         </div>
-      </div>
+      </GenContainer>
+
+      {/* Projects Container */}
+
+      <GenContainer title='Projects' icon={<FcBriefcase size={40}/>}>
+        Hi, words typing because I want to see how this looks for now. I will fill this in 
+        with words later. I just want to see how this looks. Okay so I'm just going to put more words here 
+        until I am satisfied. Done. Hi, words typing because I want to see how this looks for now. I will fill this in 
+      </GenContainer>
+
+      <GenContainer title='Contact' icon={<FcFeedback size={40}/>}>
+        Feel free to leave me a message here or email me at: reeves.abbas@gmail.com
+        <div className='flex flex-col pt-7 space-y-7'>
+          <input className='dark:bg-[#36313C] bg-lightestPurple p-2 text-lg rounded-sm' placeholder='Email'/>
+          <input className='dark:bg-[#36313C] bg-lightestPurple p-2 text-lg rounded-sm' placeholder='Subject'/>
+          <textarea className='dark:bg-[#36313C] bg-lightestPurple p-2 text-lg rounded-sm' placeholder='Message'/>
+          <button className='mx-auto font-fjalla text-3xl text-darkestPurple dark:text-white hover:scale-105 active:translate-y-0.5 ease-in-out duration-150
+            hover:bg-blue hover:border-blue dark:hover:bg-darkOrange dark:hover:border-darkOrange dark:hover:text-shadow-none drop-shadow-lg text-shadow dark:text-shadow-dark
+            hover:text-shadow-none active:text-shadow-none dark:active:text-shadow-none border-2 py-2 px-6 dark:active:bg-darkerOrange dark:active:border-darkerOrange rounded-sm active:bg-darkBlue active:border-darkerBlue'>
+            SEND
+          </button>
+        </div>
+      </GenContainer>
+
 
     </div>
   )
