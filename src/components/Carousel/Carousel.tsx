4378ce0d71029a9f4a490d200b02cac6f5da8f89
Reeves <reeves.abbas@gmail.com>
2022-05-12 18:15:10 -0400

    styling changes to genCon, finished added Carousel indicator and styling changes

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,19 +1,24 @@
+import { useTheme } from 'next-themes';
 import React, { useMemo, useState } from 'react'
-import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from 'react-icons/bs'
-import {GrGithub} from 'react-icons/gr'
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from 'react-icons/io'
+import {VscGithub} from 'react-icons/vsc'
 import IconButton from '../Buttons/IconButton';
 
 const PROJECTS = [
-  { title: 'Ergo',
-    icons: {icon: GrGithub,
+  { name: 'Ergo',
+    title: 'Task/Calendar App',
+    icons: {icon: VscGithub,
     link: ''},
-    body: 'Hello'
+    body: `App allows you to create a user, and login. Password is encrypted and JWT is used to authenticate. User's can create, delete, update, and view tasks.
+    Tasks can have up to numerous tags attached for filtering/organization (Tags have CRUD operations as well).`
   },
   {
-    title: 'Personal Website',
-    icons: {icon: GrGithub,
+    name: 'Personal Website',
+    title: 'React TS Website',
+    icons: {icon: VscGithub,
     link: ''},
-    body: 'Sugondese',
+    body: `My first experience creating a website was great. Tailwind is the best styling framework I've worked with. And even with my limited experience- I believe it will retain that title as I
+    continue to grow as a developer.`,
   }
 ]
 
@@ -21,33 +26,43 @@ let count = 0;
 
 const Carousel = () => {
 
-  const [index, setIndex] = useState(0);
-  const { title, icons, body } = useMemo(() => PROJECTS[index], [index])
+  const {theme, setTheme} = useTheme();
+  const [currIndex, setCurrIndex] = useState(0);
+  const { name, title, icons, body } = useMemo(() => PROJECTS[currIndex], [currIndex])
 
   const handleNextClick = () => { count = (count+1) % PROJECTS.length
-  setIndex(count);
-  };
+    setCurrIndex(count);
+  }
   const handlePrevClick = () => { 
     const length = PROJECTS.length;
-    count = (index + length - 1) % length;
-    setIndex(count);
+    count = (currIndex + length - 1) % length;
+    setCurrIndex(count);
   }
 
   return (
-    <div className='w-full h-fit select-none relative bg-[#2d2941] rounded-lg'>
+    <div className='mt-6 w-full h-fit select-none relative dark:bg-[#1d173b] bg-lightestPurple rounded-xl drop-shadow-lg'>
 
-      <div className='absolute w-full top-[110px] flex justify-between items-center'>
-        <button onClick={handlePrevClick}> <BsFillArrowLeftCircleFill size={35}/> </button>
-        <button onClick={handleNextClick}> <BsFillArrowRightCircleFill size={35}/> </button>
+      <div className='absolute w-full top-1/2 flex justify-between items-center opacity-100'>
+        <button onClick={handlePrevClick} className='drop-shadow-lg rounded-full dark:hover:bg-white hover:bg-darkestPurple'> <IoIosArrowDropleftCircle size={32} fill={`${ theme === 'dark' ? '#FE5E19' : '#006FDC'}`} /> </button>
+        <button onClick={handleNextClick} className='drop-shadow-lg rounded-full dark:hover:bg-white hover:bg-darkestPurple'> <IoIosArrowDroprightCircle size={32} fill={`${ theme === 'dark' ? '#FE5E19' : '#006FDC'}`}/> </button>
       </div>
 
-      <div className='h-[250px] flex flex-col space-y-4 items-end py-5 px-20'>
-        <h2> {title} </h2>
-        <IconButton Icon={icons.icon}> </IconButton>
+      <div className='h-fit flex flex-col space-y-4 items-end md:mx-20 mx-16 py-7 md:text-right text-center'>
+        <h2 className='w-full text-2xl md:text-3xl'> {name} </h2>
+        <div className='flex flex-row md:justify-end justify-center space-x-4 w-full'>
+          <IconButton Icon={icons.icon} size={30}/>
+          <h3> {title} </h3>
+        </div>
         <p> {body} </p>
       </div>
-      
+
+      <div className='flex absolute justify-center mx-auto flex-row space-x-1.5 w-full h-4 pt-3'>
+        {PROJECTS.map((el, index) => (
+          <div className={`w-2 h-2 rounded-full dark:bg-orange bg-blue ${currIndex === index ? 'opacity-100' : 'opacity-30'}`}/>
+        ))}
+      </div>
     </div>
+    
   )
 }
 
--- src/components/GenContainer.tsx
+++ src/components/GenContainer.tsx
@@ -12,8 +12,8 @@ interface Props {
 
 const GenContainer: React.FC<Props> = ({children, title, icon, id, ...rest}) => {
   return (
-      <section id={id} className='md:max-w-[1250px] flex mx-auto'>
-        <div className='md:w-[1000px] sm:w-full h-fit flex mx-auto dark:bg-lightestBlue bg-darkPurple dark:bg-opacity-[0.065] bg-opacity-10 rounded-lg p-10'>
+      <section id={id} className='md:max-w-[1250px] h-fit flex mx-auto'>
+        <div className='md:w-[1000px] sm:w-full h-full flex mx-auto dark:bg-lightestBlue bg-darkPurple dark:bg-opacity-[0.065] bg-opacity-10 rounded-lg p-10'>
           <div className='w-full flex flex-col justify-center space-y-5 md:text-left text-center'>
             <header className='flex flex-row justify-center md:justify-start'>
               <h1 className='text-3xl md:text-4xl font-yeseva dark:text-white'> {title} </h1>
