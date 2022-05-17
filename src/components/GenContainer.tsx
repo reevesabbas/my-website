b320816ebcf12df2c5aecce26a9634c3947390b5
Reeves <reeves.abbas@gmail.com>
2022-05-17 01:56:03 -0400

    fixed children being wrapped in p tag

--- src/components/GenContainer.tsx
+++ src/components/GenContainer.tsx
@@ -7,19 +7,20 @@ interface Props {
   title: string;
   icon?: IconBaseProps;
   children: React.ReactNode,
+  body?: string;
   id?: string;
 }
 
 const GenContainer: React.FC<Props> = ({children, title, icon, id, ...rest}) => {
   return (
-      <section id={id} className='md:max-w-[1250px] h-fit flex mx-auto'>
+      <section id={id} className='md:max-w-[1250px] w-full h-fit flex mx-auto'>
         <div className='md:w-[1000px] sm:w-full h-full flex mx-auto dark:bg-lightestBlue bg-darkPurple dark:bg-opacity-[0.065] bg-opacity-10 rounded-lg p-10'>
           <div className='w-full flex flex-col justify-center space-y-5 md:text-left text-center'>
             <header className='flex flex-row justify-center md:justify-start'>
               <h1 className='text-3xl md:text-4xl font-yeseva dark:text-white'> {title} </h1>
               <div className='w-fit h-fit pl-4 dark:text-orange text-blue'> <> {icon} </> </div>
             </header>
-            <p> {children} </p>
+            {children}
           </div>
         </div>
       </section>  
