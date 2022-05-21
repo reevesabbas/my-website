004e8d66116b26d66b84249543b32ab64c592621
Reeves <reeves.abbas@gmail.com>
2022-05-21 16:38:19 -0400

    removed head meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,6 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class'>
-      <head>
-        <meta charSet='UTF-8' />
-        <meta name='application-name' content='reevesabbas.com'/>
-        <meta name='author' content='Reeves Abbas | Full Stack Developer'/>
-        <meta name='description' content='Reeves Abbas is a Full-Stack Software Engineer that works with
-        numerous frameworks and languages such as- JS, CSS, HTML5 and React. Click to learn more.' />
-      </head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
