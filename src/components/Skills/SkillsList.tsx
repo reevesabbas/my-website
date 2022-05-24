f0b66364acf7a77493f537ae1a360f5a5d931a0d
Reeves <reeves.abbas@gmail.com>
2022-05-24 15:59:35 -0400

    removed react-spring and animated with tailwind

--- src/components/Skills/SkillsList.tsx
+++ src/components/Skills/SkillsList.tsx
@@ -1,7 +1,5 @@
-import React, { createElement, useState } from 'react'
-import { SiExpress, SiHtml5, SiJest, SiReact, SiStyledcomponents, SiTailwindcss, SiTypescript } from 'react-icons/si';
-import { useSpring, animated } from 'react-spring';
-import Skills from './assets';
+import React from 'react'
+import SKILLS from './assets';
 
 interface Props {
   size: number,
@@ -9,29 +7,19 @@ interface Props {
 }
 
 const SkillsList: React.FC<Props> = ({ size, onClick, ...rest}) => {
-
-  const [isHover, setisHover] = useState(false);
-
-  const props = useSpring({
-    scale: isHover ? 1.2 : 1
-  })
-
-  return (
-    <ul className='md:mx-10 flex flex-wrap text-center justify-center space-x-8 p-4'>
-      {Skills.map((skill) => {
-        const Icon = skill.icon
-        return (
-            <a href={skill.link} target='_blank' rel='noopener noreferrer' key={`${skill.name}`} className='flex justify-center'>
-              <animated.li className={`flex flex-col p-3 my-3 flex items-center justify-center w-[130px] h-55 rounded-md hover:bg-[#70758b]/20`}
-              style={props}
-              onMouseEnter={() => setisHover(true)} 
-              onMouseLeave={() => setisHover(false)}>
-                  <Icon size={size} fill={skill.fill}/>
-                <p className='mt-3 w-32 text-lg'> {skill.name} </p>
-              </animated.li>
-            </a>
-        )
-      })}
+    
+    return (
+      <ul className='md:mx-10 flex flex-wrap text-center justify-center sm:space-x-4 p-4'>
+        {SKILLS.map((el) => (
+          <a href={el.link} target='_blank' rel='noopener noreferrer' key={`${el.name}`} className='flex justify-center'>
+            <li className={`flex flex-col sm:p-2 p-2 mt-2 flex items-center justify-center w-[130px] h-55 rounded-md hover:bg-[#5a617e]/20 scale-90 hover:scale-105 ease-in-out duration-300 active:translate-y-2`}>
+              <>
+                {React.createElement(el.icon, {size: size, color: el.fill})}
+                <p className='mt-2 w-28 pt-2 text-lg'> {el.name} </p>
+              </>
+            </li>
+          </a>
+        ))}
     </ul>
   )
 }
