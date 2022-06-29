aa8b2be1903389a410284949687d8a1b2ffc812b
Reeves <reeves.abbas@gmail.com>
2022-06-28 20:13:51 -0400

    changed to headless forms and required fields

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -4,11 +4,15 @@ import FormBtn from './FormBtn'
 const ContactForm = () => {
 
   return (
-    <form method='POST' action='https://getform.io/f/af679d38-c262-47ee-b94f-1ce4f93b7611' className='flex flex-col pt-7 space-y-5 drop-shadow-md'>
-      <input name='Name' placeholder='Name'/>
-      <input name='Email' placeholder='Email'/>
-      <input name='Subject' placeholder='Subject'/>
-      <textarea name='Message' placeholder='Message'/>
+    <form 
+      method='POST' 
+      action='https://app.headlessforms.cloud/api/v1/form-submission/l49D1dmrVN' 
+      className='flex flex-col pt-7 space-y-5 drop-shadow-md'
+    >
+      <input name='Name' placeholder='Name' required />
+      <input name='Email' placeholder='Email' required />
+      <input name='Subject' placeholder='Subject' required />
+      <textarea name='Message' placeholder='Message' required />
       <FormBtn>
         SEND
       </FormBtn>
