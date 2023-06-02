903f7764b14f641ba3c506a650f43dd1e26d1773
Reeves Abbas <reeves.abbas@gmail.com>
2023-06-02 01:42:14 -0400

    social icons sizing smaller

--- src/components/Buttons/SocialButtons.tsx
+++ src/components/Buttons/SocialButtons.tsx
@@ -6,13 +6,13 @@ const SocialButtons = () => {
   return (
     <div className="flex flex-row md:justify-start justify-center space-x-9">
       <a href="https://twitter.com/reevfs" target="_blank" rel="noopener noreferrer">
-        <IconButton Icon={GrTwitter} size={45}></IconButton>
+        <IconButton Icon={GrTwitter} size={40}></IconButton>
       </a>
       <a href="https://github.com/reevesabbas" target="_blank" rel="noopener noreferrer">
-        <IconButton Icon={GrGithub} size={48}></IconButton>
+        <IconButton Icon={GrGithub} size={40}></IconButton>
       </a>
-      <a href="https://www.instagram.com/reeves.19/" target="_blank" rel="noopener noreferrer">
-        <IconButton Icon={GrInstagram} size={45}></IconButton>
+      <a href="https://www.instagram.com/reeves.ab/" target="_blank" rel="noopener noreferrer">
+        <IconButton Icon={GrInstagram} size={40}></IconButton>
       </a>
     </div>
   );
