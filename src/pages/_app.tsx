7d53c027fb2a6ce80b0c6579671ade044b5a0bc8
Reeves Abbas <reeves.abbas@gmail.com>
2025-05-23 22:54:43 -0400

    added image preloader for carousel images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,31 @@ import { ThemeProvider } from 'next-themes';
 
 import '../styles/globals.css';
 
+import Image from 'next/image';
+import { HOBBIES } from '../components/HobbyList/assets';
+
+const ImagePreloader = () => {
+  return (
+    <div style={{ display: 'none' }}>
+      {Object.values(HOBBIES).flat().map((hobby, index) => (
+        <Image
+          key={`preload-${index}`}
+          src={hobby.img}
+          alt=""
+          width={1}
+          height={1}
+          priority={true}
+        />
+      ))}
+    </div>
+  );
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <ImagePreloader />
     </ThemeProvider>
   );
 }
